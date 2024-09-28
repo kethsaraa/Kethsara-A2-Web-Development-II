d5e626e40fe1ab5025d578eff45a8bb8741842b7
kethsaraa <kethsarakothalawala@gmail.com>
2024-09-29 03:00:19 +0800

    Create crowdfunding_db_fundraiser.sql

---
+++ MySql Files/crowdfunding_db_fundraiser.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: crowdfunding_db
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fundraiser`
+--
+
+DROP TABLE IF EXISTS fundraiser;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE fundraiser (
+  FUNDRAISER_ID int NOT NULL AUTO_INCREMENT,
+  ORGANIZER varchar(255) DEFAULT NULL,
+  CAPTION text,
+  TARGET_FUNDING decimal(10,2) DEFAULT NULL,
+  CURRENT_FUNDING decimal(10,2) DEFAULT NULL,
+  CITY varchar(255) DEFAULT NULL,
+  `ACTIVE` tinyint(1) DEFAULT NULL,
+  CATEGORY_ID int DEFAULT NULL,
+  PRIMARY KEY (FUNDRAISER_ID),
+  KEY CATEGORY_ID (CATEGORY_ID),
+  CONSTRAINT fundraiser_ibfk_1 FOREIGN KEY (CATEGORY_ID) REFERENCES category (CATEGORY_ID)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fundraiser`
+--
+
+LOCK TABLES fundraiser WRITE;
+/*!40000 ALTER TABLE fundraiser DISABLE KEYS */;
+INSERT INTO fundraiser VALUES (1,'The Ahmetaj Family','Support Milena\'s Fight Against Leukemia',300000.00,150000.00,'Sarasota, FL',1,1),(2,'Brian Johannesen','Ashlee Hopkins Memorial Fund',200000.00,100000.00,'Iowa City, IA',1,2),(3,'William Carroll','Support Devin\'s Recovery',50000.00,30000.00,'Shepherdstown, WV',1,3);
+/*!40000 ALTER TABLE fundraiser ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-29  2:59:17
