478df0ae8dffbe01b86e179a1c3409b8e75a1b79
kethsaraa <kethsarakothalawala@gmail.com>
2024-09-28 23:02:35 +0800

    test route to check database connection

--- crowdfunding_api/server.js
+++ crowdfunding_api/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const db = require('./crowdfunding_db');
 const app = express();
+
+app.get('/test', (req, res) => {
+    db.query('SELECT * FROM FUNDRAISER', (err, results) => {
+        if (err) {
+            return res.status(500).send('Error fetching data');
+        }
+        res.json(results);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
