f1712110b911d2162999195b424a336b92960b0b
kethsaraa <kethsarakothalawala@gmail.com>
2024-09-28 23:39:33 +0800

    Update crowdfunding_db.js

--- crowdfunding_api/crowdfunding_db.js
+++ crowdfunding_api/crowdfunding_db.js
@@ -1,18 +1,20 @@
 const mysql = require('mysql2');
 
+// Create a connection to the database
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'your_password',
-    database: 'crowdfunding_db'
+    user: 'root', 
+    password: '1234', 
+    database: 'crowdfunding_db' 
 });
 
+// Connect to MySQL
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
-    } else {
-        console.log('Connected to the database');
+        return;
     }
+    console.log('Connected to the database');
 });
 
 module.exports = db;
